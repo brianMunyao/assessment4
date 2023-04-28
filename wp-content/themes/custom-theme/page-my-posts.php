cc19f175345a1b728b8b244e4bee3ded066f2370
brianMunyao <brianmunyao6@gmail.com>
2023-04-28 11:28:18 +0300

    added db

--- wp-content/themes/custom-theme/page-my-posts.php
+++ wp-content/themes/custom-theme/page-my-posts.php
@@ -10,24 +10,31 @@
     <h1>Posts Page</h1>
 
     <?php
-
-    $blogs = new WP_Query('type=post&posts_per_page=3');
+    $blogs = new WP_Query('type=post');
 
     if ($blogs->have_posts()) :
         while ($blogs->have_posts()) : $blogs->the_post(); ?>
-
-
 
             <?php get_template_part('content', get_post_format()) ?>
 
     <?php
         endwhile;
     endif;
+
+    // previous_posts_link('&laquo; Newer posts');
+    // next_posts_link('Older posts &raquo;');
+    ?>
+
+    <div><?php //previous_posts_link('Older posts'); 
+            ?></div>
+    <div><?php // next_posts_link('Newer posts'); 
+            ?></div>
+
+    <?php
     wp_reset_postdata();
     ?>
 
 
-    <div class="nav-previous alignleft"><?php previous_posts_link('Older posts'); ?></div>
-    <div class="nav-next alignright"><?php next_posts_link('Newer posts'); ?></div>
+
     <?php get_footer(); ?>
 </div>
